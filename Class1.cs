271f5cbc1bf96df731fdf02d93b6853f4ebac502
Sanya <sanya.gupta@celebaltech.com>
2023-08-23 08:26:01 +0530

    first commit

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace hello
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("helo");
+        }
+    }
+}
+
---
+++ Conditional statements/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace Conditional_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //if else statement//
+            /*     Console.WriteLine("enter your age:");
+                  int age = int.Parse(Console.ReadLine());
+               if (age >= 18)
+               {
+                   Console.WriteLine("You are an adult.");
+               }
+               else
+               {
+                   Console.WriteLine("You are not an adult.");
+               } */
+
+
+
+            //if-else if statement//
+            /*    Console.WriteLine("enter your marks:");
+                int marks = Convert.ToInt32(Console.ReadLine());
+                if (marks >= 90)
+                {
+                    Console.WriteLine("Grade A");
+                }
+                else if(marks>=70 && marks<=80)
+                {
+                    Console.WriteLine("Grade B");
+                }
+                else if(marks>40 && marks<70)
+                {
+                    Console.WriteLine("Grade C");
+                }
+                else
+                {
+                    Console.WriteLine("Grade D");
+                }*/
+
+
+
+
+            //Switch statement//
+            /*  Console.WriteLine("Choose a fruit: (apple, banana, orange)");
+               string fruit = Console.ReadLine();
+
+               switch (fruit)
+               {
+                   case "apple":
+                       {
+                           Console.WriteLine("You picked an apple.");
+                           break;
+                       }
+                   case "banana":
+                       {
+                           Console.WriteLine("You picked a banana.");
+                           break;
+                       }
+                   case "orange":
+                       {
+                           Console.WriteLine("You picked an orange.");
+                           break;
+                       }
+                   default:
+                       {
+                           Console.WriteLine("Please enter a valid fruit choice.");
+                           break;
+                       }
+               }*/
+
+
+
+
+
+            //nested switch//
+            /*   Console.WriteLine("enter the gender of a person Men or Women");
+               string gender = Console.ReadLine();
+               Console.WriteLine("enter the marital status of a person Married or UnMarried");
+               string MartialStatus = Console.ReadLine();
+               switch (gender)
+               {
+                   case "Men":
+                       switch (MartialStatus)
+                       {
+                           case "Married":
+                               Console.WriteLine("The Person is Men and Married");
+                               break;
+
+                           case "UnMarried":
+                               Console.WriteLine("The Person is Men and UnMarried");
+                               break;
+                       }
+                       break;
+
+                   case "Women":
+                       switch (MartialStatus)
+                       {
+                           case "Married":
+                               Console.WriteLine("The Person is Women and Married");
+                               break;
+
+                           case "UnMarried":
+                               Console.WriteLine("The Person is Women and UnMarried");
+                               break;
+                       }
+                       break;
+
+                   default:
+                       Console.WriteLine("The person is not  Men or Women");
+                       break;
+               }
+            */
+
+
+
+
+            // for loop//
+            int i = 0;
+              for (; i < 5;)
+                  {
+                       
+                Console.WriteLine(i);
+               
+                  } 
+
+
+
+            //while loop//
+           /* int i = 0;
+            while(i<5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }  */
+
+
+
+
+            //do while loop//
+            /*  int i = 0;
+              do
+              {
+                  Console.WriteLine(i);
+                  i++;
+              }
+              while (i < 5); */
+
+
+
+
+        }
+        }
+    }
